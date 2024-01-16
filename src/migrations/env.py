4342555a9bb9417777953d098a6efd122bf071ff
semyon nakrokhin <2206095@gmail.com>
2024-01-16 19:31:05 +0300

    linter corrections for src/migrations

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -22,13 +22,16 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-config.set_main_option("sqlalchemy.url", Settings().database.dsn + "?async_fallback=True")
+config.set_main_option(
+    "sqlalchemy.url", Settings().database.dsn + "?async_fallback=True"
+)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 from models.entities import WeatherORMModel  # noqa
+
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
@@ -75,9 +78,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
